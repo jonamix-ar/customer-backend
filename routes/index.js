f1f45e0f6ac17b7e6a48bec5e834a7e4cb3fc959
JonaMiX <jonatanvilella@gmail.com>
2024-04-22 14:48:31 -0300

    Update router.js
    Update router.js for deploy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,23 @@ const authController = require("../controllers/authController");
 const consultationController = require("../controllers/consultationController");
 const verifyToken = require("../middlewares/validate-token");
 
+// Rutas de pruebas
+app.get("/", (req, res) => {
+  // Accede a la variable de entorno VERSION
+  const version = process.env.VERSION;
+
+  // Verifica si la variable de entorno VERSION está definida
+  if (version) {
+    // Envía la versión como JSON
+    res.json({ version });
+  } else {
+    // Si la variable de entorno no está definida, envía un mensaje de error
+    res
+      .status(500)
+      .json({ error: "La variable de entorno VERSION no está definida." });
+  }
+});
+
 // Rutas de autenticación
 router.post("/login", authController.login);
 router.post("/register", authController.register);
